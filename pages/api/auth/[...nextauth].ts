2ddcf957e5548b915274d5feaa83792608fc1f86
aimunNaharr <aimunnaharr@gmail.com>
2023-04-15 20:04:36 +0600

    comments added for authtication handler

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,13 +1,18 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import NextAuth, { AuthOptions } from "next-auth";
 import prisma from "@/libs/prismadb";
+// Import providers for GitHub, Google and credentials
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
+// Import bcrypt for password hashing and comparison
 import bcrypt from 'bcrypt'
 
+// Define the auth options for NextAuth
 export const authOptions: AuthOptions = {
+	// Use the Prisma adapter with the prisma client instance
 	adapter: PrismaAdapter(prisma),
+	// Specify the providers for authentication
 	providers: [
 		GithubProvider({
 			clientId: process.env.GITHUB_ID as string,
@@ -17,6 +22,7 @@ export const authOptions: AuthOptions = {
 			clientId: process.env.GOOGLE_CLIENT_ID as string,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
 		}),
+		// Credentials provider with email and password fields
 		CredentialsProvider({
 			name: "credentials",
 			credentials: {
@@ -24,21 +30,26 @@ export const authOptions: AuthOptions = {
 				password: { label: "password", type: "password" },
 			},
 			async authorize(credentials) {
+				// Check if the credentials are valid
 				if (!credentials?.email || !credentials?.password) {
 					throw new Error("Invalid credentials");
 				}
+				// Find the user with the given email in the database
 				const user=await prisma.user.findUnique({
 					where:{
 						email: credentials.email
 					}
 				})
+				// Check if the user exists and has a hashed password
 				if(!user || !user?.hashPassword){
 					throw new Error('Invalid credentials')
 				}
+				// Compare the given password with the hashed password
 				const isCorrectPassword=await bcrypt.compare(credentials.password, user.hashPassword)
 				if(!isCorrectPassword){
 					throw new Error('Invalid credentials')
 				}
+				// Return the user object if authenticated successfully
 				return user
 			},
 		}),
@@ -54,4 +65,5 @@ export const authOptions: AuthOptions = {
 	secret: process.env.NEXTAUTH_SECRET
 };
 
+// Export the default handler for NextAuth with the auth options
 export default NextAuth(authOptions)
